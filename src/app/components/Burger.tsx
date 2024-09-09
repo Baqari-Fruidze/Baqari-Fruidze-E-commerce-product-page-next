7d92bc421ad068c00994dbb45588d0e62490bb79
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-09-09 20:39:03 +0400

    zoostand

--- src/app/components/Burger.tsx
+++ src/app/components/Burger.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Image from "next/image";
+import useBurgerShow from "@/usestore/BurgerShow";
 
 export default function Burger() {
+  const burgerShow = useBurgerShow();
   return (
-    <div className="Cover bg-[0,0,0,0.4] w-full h-full z-20">
-      <div className="bg-[#fff] w-1/2 flex flex-col gap-[53px]">
+    <div className="Cover bg-[rgba(0,0,0,0.4)] w-full min-h-screen fixed top-0">
+      <div className=" min-h-screen bg-[#fff] w-3/5 flex flex-col gap-[53px] pl-[24px] pt-[24px]">
         <Image
           src={"/images/icon-close.svg"}
           width={13.4}
           height={13.4}
           alt="close icon"
+          onClick={() => burgerShow.setTrue()}
         ></Image>
-        <div className="textCon flex flex-col gap-[20px] ">
-          <span className="text-[#1D2026] text-[18px] font-bold leading-[26px]">
+        <div className="textCon flex flex-col gap-[20px]  ">
+          <span className="text-[#red] text-[18px] font-bold leading-[26px]">
             Collections
           </span>
           <span className="text-[#1D2026] text-[18px] font-bold leading-[26px]">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Burger from "./components/Burger";
 
 export default function Home() {
-  return (
-    <div>
-      <Burger />
-    </div>
-  );
+  return <div></div>;
 }
